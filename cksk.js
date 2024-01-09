01084d701ad7e654ec3607b4e58aeb977fa1ae4c
csk666 <62277551+csk666@users.noreply.github.com>
2024-01-09 23:13:36 +0800

    Update cksk.js

--- cksk.js
+++ cksk.js
@@ -1,84 +1,55 @@
-const cookieName = "cksk";
-const cookieKey = "jsessionId";
-const chavy = init();
-
-let isGetCookie = typeof $request !== "undefined";
-chavy.log(`[${cookieName}] 是否获取cookie: ${isGetCookie}`)
-if (isGetCookie) {
-  getcookie();
-} else {
-  sign();
-}
-
-function getcookie() {
-  var cookieVal = $request.headers["Cookie"];
-  if (cookieVal) {
-    var matchResult = cookieVal.match(/(?<=JSESSIONID=).*/gi);
-    if (0 !== matchResult.length) {
-      var jsessionId = matchResult[0];
-      if (jsessionId && chavy.getdata(cookieKey)) {
-        jsessionId !== chavy.getdata(cookieKey)
-          ? (chavy.setdata(jsessionId, cookieKey),
-            chavy.msg(`${cookieName}`, "更新jsessionId: 成功", ""))
-          : chavy.msg(`${cookieName}`, "目前的jsessionId一致", "");
-      } else {
-        jsessionId != undefined
-          ? (chavy.setdata(jsessionId, cookieKey),
-            chavy.msg(`${cookieName}`, "获取jsessionId: 成功", ""))
-          : chavy.msg(`${cookieName}`, "获取jsessionId: 失败", "");
-      }
-    } else {
-      chavy.msg(`${cookieName}`, "cookie中匹配jsessionId: 失败", "");
-    }
-  } else {
-    chavy.msg(`${cookieName}`, "获取cookie: 失败", "");
+const cookieName = 'AcFun'
+const cookieKey = 'jsessionId'
+const chavy = init()
+const cookieVal = $request.headers['Cookie']
+if (cookieVal) {
+  if (chavy.setdata(cookieVal, cookieKey)) {
+    chavy.msg(`${cookieName}`, '获取Cookie: 成功', '')
+    chavy.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
   }
 }
 
-function sign() {}
-
 function init() {
   isSurge = () => {
-    return undefined === this.$httpClient ? false : true;
-  };
+    return undefined === this.$httpClient ? false : true
+  }
   isQuanX = () => {
-    return undefined === this.$task ? false : true;
-  };
+    return undefined === this.$task ? false : true
+  }
   getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key);
-    if (isQuanX()) return $prefs.valueForKey(key);
-  };
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
   setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val);
-    if (isQuanX()) return $prefs.setValueForKey(key, val);
-  };
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
   msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body);
-    if (isQuanX()) $notify(title, subtitle, body);
-  };
-  log = (message) => console.log(message);
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
   get = (url, cb) => {
     if (isSurge()) {
-      $httpClient.get(url, cb);
+      $httpClient.get(url, cb)
     }
     if (isQuanX()) {
-      url.method = "GET";
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body));
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
-  };
+  }
   post = (url, cb) => {
     if (isSurge()) {
-      $httpClient.post(url, cb);
+      $httpClient.post(url, cb)
     }
     if (isQuanX()) {
-      url.method = "POST";
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body));
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
-  };
+  }
   done = (value = {}) => {
-    $done(value);
-  };
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done };
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
 }
-
-chavy.done();
+chavy.done()
